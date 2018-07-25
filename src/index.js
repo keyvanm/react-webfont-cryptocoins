01800ecd92ee694361ab6cb6f46043414dc7a49b
Keyvan Mosharraf <kayvanmsh@gmail.com>
2018-07-25 15:45:12 -0700

    Changed the way the component is rendered so you can add classnames to it and whatnot. might not be a good idea

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import React from 'react';
 import 'cryptocoins-icons/webfont/cryptocoins.css'
 import 'cryptocoins-icons/webfont/cryptocoins-colors.css'
 
-export default ({ coin }) => (
-  <i className={`cc ${coin}`} title={coin}></i>
-)
+export default ({ coin, ...props }) => {
+  let className = `cc ${coin}`;
+  if (props.className) {
+    className += " " + props.className
+  }
+  return (
+    <i {...props} className={className} title={coin}></i>
+  )
+}
